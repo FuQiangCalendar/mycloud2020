b2903c518597616717c40867371317910272ad5b
FuQiangCalendar <fq2295827430@126.com>
2020-09-04 17:37:08 +0800

    新增selenium-spider模块

---
+++ cloud-spider-selenium8090/src/main/java/com/atguigu/springcloud/utils/ChromeDriverUtil.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud.utils;
+
+
+public class ChromeDriverUtil {
+	
+	static {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver_win32\\chromedriver.exe");
+	}
+	
+	
+}
