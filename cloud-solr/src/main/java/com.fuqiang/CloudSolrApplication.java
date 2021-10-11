0f82cb807e3bb686a1dc2925c79a0e000822bebf
FuQiangCalendar <fq2295827430@126.com>
2021-10-11 17:10:00 +0800

    提交springboot+solr

---
+++ cloud-solr/src/main/java/com.fuqiang/CloudSolrApplication.java
@@ -0,0 +1,18 @@
+package com.fuqiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FuQiangCalendar
+ * @title: CloudSolrApplication
+ * @projectName cloud2020
+ * @description: springboot 集成solr
+ * @date 2021/10/11 13:45
+ */
+@SpringBootApplication
+public class CloudSolrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSolrApplication.class, args);
+    }
+}
